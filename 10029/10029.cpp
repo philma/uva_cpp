aa7c86397dac10204dfccfaf96a1bd3cbdc5da4e
philma <1178188984@qq.com>
2016-02-05 17:08:35 +0800

    Edit Step Ladders

--- 10029/10029.cpp
+++ 10029/10029.cpp
@@ -6,56 +6,63 @@
 
 using namespace std;
 
+void generate_words(const string& word, vector<string>& next_words)
+{
+    if(word.size() > 1)
+    {
+        for(unsigned int i = 0; i < word.size(); ++i)
+        {
+            string tmp = word;
+            tmp.erase(i, 1);
+            if(tmp <= word) continue;
+            next_words.push_back(tmp);
+        }
+    }
+
+    for(unsigned int i = 0; i < word.size(); ++i)
+    {
+        for(char j = 'a'; j <= 'z'; ++j)
+        {
+            string tmp = word;
+            tmp[i] = j;
+            if(tmp <= word) continue;
+            next_words.push_back(tmp);
+        }
+    }
+
+    for(unsigned int i = 0; i < word.size(); ++i)
+    {
+        for(char j = 'a'; j <= 'z'; ++j)
+        {
+            string tmp = word;
+            tmp.insert(i, 1, j);
+            if(tmp <= word) continue;
+            next_words.push_back(tmp);
+        }
+    }
+
+    for(char i = 'a'; i <= 'z'; ++i)
+    {
+        string tmp = word;
+        tmp += i;
+        if(tmp <= word) continue;
+        next_words.push_back(tmp);
+    }
+}
+
 void init_indegree(map<string, unsigned short>& words)
 {
     map<string, unsigned short>::iterator it = words.begin();
     for(; it != words.end(); ++it)
     {
         const string& word = it->first;
+        vector<string> next_words;
+        generate_words(word, next_words);
 
-        if(word.size() > 1)
+        for(unsigned int i = 0; i < next_words.size(); ++i)
         {
-            for(unsigned int i = 0; i < word.size(); ++i)
-            {
-                string tmp = word;
-                tmp.erase(i, 1);
-                if(tmp <= word) continue;
-                if(words.find(tmp) != words.end())
-                    ++(words[tmp]);
-            }
-        }
-
-        for(unsigned int i = 0; i < word.size(); ++i)
-        {
-            for(char j = 'a'; j <= 'z'; ++j)
-            {
-                string tmp = word;
-                tmp[i] = j;
-                if(tmp <= word) continue;
-                if(words.find(tmp) != words.end())
-                    ++(words[tmp]);
-            }
-        }
-
-        for(unsigned int i = 0; i < word.size(); ++i)
-        {
-            for(char j = 'a'; j <= 'z'; ++j)
-            {
-                string tmp = word;
-                tmp.insert(i, 1, j);
-                if(tmp <= word) continue;
-                if(words.find(tmp) != words.end())
-                    ++(words[tmp]);
-            }
-        }
-
-        for(char i = 'a'; i <= 'z'; ++i)
-        {
-            string tmp = word;
-            tmp += i;
-            if(tmp <= word) continue;
-            if(words.find(tmp) != words.end())
-               ++(words[tmp]);
+            if(words.find(next_words[i]) != words.end())
+               ++(words[next_words[i]]);
         }
     }
 }
@@ -70,64 +77,16 @@ unsigned int find_answer(map<string, unsigned short>& words, vector<string>& zer
         for(unsigned int i = 0; i < zero_indegree.size(); ++i)
         {
             const string& word = zero_indegree[i];
-            if(word.size() > 1)
-            {
-                for(unsigned int pos = 0; pos < word.size(); ++pos)
-                {
-                    string tmp_word = word;
-                    tmp_word.erase(pos, 1);
-                    if(tmp_word <= word) continue;
-                    if(words.find(tmp_word) != words.end())
-                    {
-                        --(words[tmp_word]);
-                        if(words[tmp_word] == 0)
-                            tmp.push_back(tmp_word);
-                    }
-                }
-            }
+            vector<string> next_words;
+            generate_words(word, next_words);
 
-            for(unsigned int pos = 0; pos < word.size(); ++pos)
+            for(unsigned int j = 0; j < next_words.size(); ++j)
             {
-                for(unsigned char j = 'a'; j <= 'z'; ++j)
+                if(words.find(next_words[j]) != words.end())
                 {
-                    string tmp_word = word;
-                    tmp_word[pos] = j;
-                    if(tmp_word <= word) continue;
-                    if(words.find(tmp_word) != words.end())
-                    {
-                        --(words[tmp_word]);
-                        if(words[tmp_word] == 0)
-                            tmp.push_back(tmp_word);
-                    }
-                }
-            }
-
-            for(unsigned int pos = 0; pos < word.size(); ++pos)
-            {
-                for(unsigned char j = 'a'; j <= 'z'; ++j)
-                {
-                    string tmp_word = word;
-                    tmp_word.insert(pos, 1, j);
-                    if(tmp_word <= word) continue;
-                    if(words.find(tmp_word) != words.end())
-                    {
-                        --(words[tmp_word]);
-                        if(words[tmp_word] == 0)
-                            tmp.push_back(tmp_word);
-                    }
-                }
-            }
-
-            for(unsigned char j = 'a'; j <= 'z'; ++j)
-            {
-                string tmp_word = word;
-                tmp_word += j;
-                if(tmp_word <= word) continue;
-                if(words.find(tmp_word) != words.end())
-                {
-                    --(words[tmp_word]);
-                    if(words[tmp_word] == 0)
-                        tmp.push_back(tmp_word);
+                    --(words[next_words[j]]);
+                    if(words[next_words[j]] == 0)
+                        tmp.push_back(next_words[j]);
                 }
             }
 
