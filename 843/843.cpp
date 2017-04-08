95a94931a35c0fde3442410d230afb56e4f241de
philma <1178188984@qq.com>
2017-04-08 13:52:52 +0800

    Crypt Kicker

---
+++ 843/843.cpp
@@ -0,0 +1,115 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cstdio>
+
+static const unsigned int ALPHABET_LEN = 26;
+
+using namespace std;
+
+void split_by_space(const string& line, vector<string>& words)
+{
+    size_t pos = 0, begin_pos = 0;
+    while((pos = line.find(' ', begin_pos)) 
+            != string::npos)
+    {
+        string tmp = line.substr(begin_pos, pos - begin_pos);
+        if(tmp != "")
+            words.push_back(tmp);
+        begin_pos = pos + 1;
+    }
+
+    if(begin_pos < line.size())
+        words.push_back(line.substr(begin_pos, line.length() - begin_pos));
+}
+
+void decrypt(const vector<string>& dict, const vector<string>& words
+        , size_t index, string& alpha_b1, string& alpha_b2, bool& find)
+{
+    if(index >= words.size())
+    {
+        if(index) find = true;
+        return ;
+    }
+
+    for(size_t i = 0; i < dict.size(); ++i)
+    {
+        if(words[index].size() != dict[i].size())
+            continue;
+
+        const string& word = words[index];
+        const string& d_word = dict[i];
+        string ab1_bak(alpha_b1), ab2_bak(alpha_b2);
+        bool match = true;
+        for(size_t j = 0; j < word.size(); ++j)
+        {
+            if(ab1_bak[d_word[j] - 'a'] == '0'
+                    && ab2_bak[word[j] - 'a'] == '0')
+            {
+                ab1_bak[d_word[j] - 'a'] = word[j];
+                ab2_bak[word[j] - 'a'] = d_word[j];
+            }
+            else if(ab1_bak[d_word[j] - 'a'] == word[j]
+                    && ab2_bak[word[j] - 'a'] != '0')
+                continue;
+            else
+            {
+                match = false;
+                break;
+            }
+        }
+
+        if(match)
+        {
+            swap(ab1_bak, alpha_b1);
+            swap(ab2_bak, alpha_b2);
+            decrypt(dict, words, index + 1, alpha_b1, alpha_b2, find);
+            if(find)
+                break;
+            else
+            {
+                swap(ab1_bak, alpha_b1);
+                swap(ab2_bak, alpha_b2);
+            }
+        }
+        else
+            continue;
+    }
+}
+
+int main()
+{
+    vector<string> dict;
+    int n = 0;
+    cin >> n;
+    getchar();
+    for(int i = 0; i < n; ++i)
+    {
+        string tmp;
+        getline(cin, tmp);
+        dict.push_back(tmp);
+    }
+
+    string line;
+    while(getline(cin, line))
+    {
+        string alpha_b1(ALPHABET_LEN, '0');
+        string alpha_b2(ALPHABET_LEN, '0');
+        bool find = false;
+
+        vector<string> words;
+        split_by_space(line, words);
+        decrypt(dict, words, 0, alpha_b1, alpha_b2, find);
+
+        for(size_t i = 0; i < line.size(); ++i)
+        {
+            if(find)
+                cout << (line[i] == ' ' ? ' ' : alpha_b2[line[i] - 'a']);
+            else
+                cout << (line[i] == ' ' ? ' ' : '*');
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
