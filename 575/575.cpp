d16b8ea0a80938e39d475e46e95a9e532983c32e
philma <1178188984@qq.com>
2017-04-16 15:00:59 +0800

    Skew Binary

---
+++ 575/575.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    auto decimal = [](const string& str){
+        auto sum = 0;
+        auto len = str.length();
+        
+        for(auto i = 0; i < len; ++i)
+            sum += (pow(2, len - i) - 1) * (str[i] - '0');
+        
+        return sum;
+    };
+    
+    string skew_b;
+    while(getline(cin, skew_b) && skew_b != "0")
+        cout << decimal(skew_b) << endl;
+    
+    return 0;
+}
