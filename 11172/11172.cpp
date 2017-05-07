90e920ff84a01ac59699157eb9cbb33adf5b1106
philma <1178188984@qq.com>
2017-05-07 15:55:14 +0800

    Relational Operator

---
+++ 11172/11172.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int t = 0;
+    cin >> t;
+    while(t--)
+    {
+        int a = 0, b = 0;
+        cin >> a >> b;
+        if(a > b)
+            cout << '>';
+        else if(a < b)
+            cout << '<';
+        else
+            cout << '=';
+        cout << endl;
+    }
+
+    return 0;
+}
